a222db8ee2cc4693e6553cd1b45f6ec91139e370
jessiegibson <jessie@thirstynyc.com>
2015-08-31 20:31:20 -0400

    Looging

--- looping.rb
+++ looping.rb
@@ -1,5 +1,5 @@
 def looping
-	#your code here
+	puts "It's your first year at Hogwarts and you're having a tough time getting the hang of that levitation spell. Fill out the content of the method `looping` to contain a `loop` that `puts` the levitation charm `\"Wingardium Leviosa\"` an infinite number of times."
 end
 
-#call your method here
+looping
